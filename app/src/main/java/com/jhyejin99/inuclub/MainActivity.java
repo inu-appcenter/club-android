191464e8ccc7b754f300a1de91f3ebc6c37fd308
jhyejin99 <vol1mond@naver.com>
2020-01-10 17:22:31 +0900

    하단바 변경

--- app/src/main/java/com/jhyejin99/inuclub/MainActivity.java
+++ app/src/main/java/com/jhyejin99/inuclub/MainActivity.java
@@ -1,11 +1,11 @@
 package com.jhyejin99.inuclub;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.jhyejin99.inuclub.Settings.Setting;
