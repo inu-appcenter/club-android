2d11a1699149e10af2534012aaa2d54281094edf
jhyejin99 <vol1mond@naver.com>
2020-01-12 15:37:24 +0900

    동아리 소개 페이지 지원하기 팝업창

--- app/src/main/java/com/jhyejin99/inuclub/RecyclerViews/RecyclerViewAdapterL.java
+++ app/src/main/java/com/jhyejin99/inuclub/RecyclerViews/RecyclerViewAdapterL.java
@@ -1,5 +1,7 @@
 package com.jhyejin99.inuclub.RecyclerViews;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,13 +11,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.jhyejin99.inuclub.Homes.ClubDetail;
 import com.jhyejin99.inuclub.R;
 
 import java.util.ArrayList;
 
 public class RecyclerViewAdapterL extends RecyclerView.Adapter<RecyclerViewAdapterL.viewHolder> {
     ArrayList<ClubListItemData>list;
-
+    Context context;
     public RecyclerViewAdapterL(ArrayList<ClubListItemData>list){
         this.list = list;
     }
@@ -24,12 +27,13 @@ public class RecyclerViewAdapterL extends RecyclerView.Adapter<RecyclerViewAdapt
     public RecyclerViewAdapterL.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cate_club_list_item,parent,false);
         RecyclerViewAdapterL.viewHolder viewHolder = new viewHolder(view);
+        context = parent.getContext();
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapterL.viewHolder holder, int position) {
-        ClubListItemData clubListItem = list.get(position);
+        final ClubListItemData clubListItem = list.get(position);
 
         holder.clubName.setText(clubListItem.getClubName());
         holder.clubPlace.setText(clubListItem.getClubPlace());
@@ -39,6 +43,10 @@ public class RecyclerViewAdapterL extends RecyclerView.Adapter<RecyclerViewAdapt
             @Override
             public void onClick(View v) {
                 //todo 해당 동아리 소개페이지
+                Intent intent = new Intent(context, ClubDetail.class);
+                intent.putExtra("동아리이름",clubListItem.getClubName());
+                intent.putExtra("동아리방",clubListItem.getClubPlace());
+                context.startActivity(intent);
             }
         });
     }
