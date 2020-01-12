3f158de2220d0e94d47ee5290bb7f0284bdbaba0
jhyejin99 <vol1mond@naver.com>
2020-01-12 17:15:40 +0900

    오류 수정

--- app/src/main/java/com/jhyejin99/inuclub/Settings/SettingClubEdit.java
+++ app/src/main/java/com/jhyejin99/inuclub/Settings/SettingClubEdit.java
@@ -1,16 +1,24 @@
 package com.jhyejin99.inuclub.Settings;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.jhyejin99.inuclub.R;
 
 public class SettingClubEdit extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_event_edit);
-
     }
+
 }
