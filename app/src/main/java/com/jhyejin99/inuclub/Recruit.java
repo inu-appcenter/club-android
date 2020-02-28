36f36f0c3af51335807c95c82cfe73021546e6c3
GHeeJeon <jhjeon_99@naver.com>
2020-02-29 01:59:14 +0900

    일단커밋

--- app/src/main/java/com/jhyejin99/inuclub/Recruit.java
+++ app/src/main/java/com/jhyejin99/inuclub/Recruit.java
@@ -1,5 +1,6 @@
 package com.jhyejin99.inuclub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,20 +8,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.jhyejin99.inuclub.RecyclerViews.RecyclerViewAdapterR;
 
 import java.util.ArrayList;
-
-//TODO fab구현 위해 edit.xml 만들기. 위... 저거를... 하늘색...그거 고정시켜야 할까...?
+import java.util.List;
 
 public class Recruit extends Fragment {
 
     TabLayout.Tab tabIng;
     TabLayout.Tab tabDone;
     RecyclerViewAdapterR adapter;
-
+    FloatingActionButton recruit_fab;
 
     public Recruit() {
     }
@@ -65,11 +70,55 @@ public class Recruit extends Fragment {
             }
         });
 
+        recruit_fab = (FloatingActionButton) root.findViewById(R.id.recruit_fab);
         // RecyclerView
-        RecyclerView rcv = root.findViewById(R.id.moim_recycler);
+        /*RecyclerView rcv = root.findViewById(R.id.moim_recycler);
         rcv.setAdapter(adapter);
 
         onMoimTabChanged(1);
+
+        //for spinner array
+        spinner = root.findViewById(R.id.spinner_member);
+        List<Integer> members = new ArrayList<>();
+        members.add(0);
+        for(int i = 5; i <= 20; i = i+5){
+            members.add(i);
+        }
+        ArrayAdapter dataAdapter;
+        dataAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, members);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if(parent.getItemAtPosition(position).equals(0)){
+                    //do nothing
+                }
+
+                else{
+                    int item = (int)parent.getItemAtPosition(position);
+
+                    Toast.makeText(parent.getContext(), item + "선택", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });*/
+    }
+
+    private void initListener() {
+        recruit_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MakeVoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void onMoimTabChanged(int tab) {
@@ -89,6 +138,8 @@ public class Recruit extends Fragment {
 
     private static final int TAB_ING = 1;
     private static final int TAB_DONE = 2;
+
+
 
 }
 
