c9c44036611147207e5035ee679b6c20b33b85dc
potados99 <potados99@gmail.com>
2021-02-20 23:17:31 +0900

    클린 아키텍쳐의 엔티티 DDD의 그것을 비교하며 끝없는 고뇌중...

---
+++ domain/src/main/java/org/inu/club/entity/ClubApplication.kt
@@ -0,0 +1,28 @@
+/**
+ * This file is part of INU Club.
+ *
+ * Copyright (C) 2021 INU Global App Center <potados99@gmail.com>
+ *
+ * INU Club is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INU Club is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.inu.club.entity
+
+/**
+ * 동아리 지원 정보
+ */
+data class ClubApplication(
+        val openChatUrl: String,
+        val websiteUrl: String
+)
